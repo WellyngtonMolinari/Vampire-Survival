71be7b8bba350093b6dba401b76d4dc5e718fd0a
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-05-05 09:17:48 -0300

    Camera and Animations update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed;
+
+    public Animator anim;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Application.targetFrameRate = 60;
+        Vector3 moveInput = new Vector3(0f, 0f, 0f);
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+
+        transform.position += moveInput * moveSpeed * Time.deltaTime;
+
+        if(moveInput != Vector3.zero)
+        {
+            anim.SetBool("isMoving", true);
+        } else 
+            {
+                anim.SetBool("isMoving", false);
+            }
     }
 }
